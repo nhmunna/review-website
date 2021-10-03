8a0ed2491f11e4f700abc8de0c9f16e14ed1d94b
nhmunna <munna1720.nhm@gmail.com>
2021-10-04 01:15:14 +0600

    added header and footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This site is copyright@NHMunna</h2>
-            <p>2021</p>
+        <div className="bg-secondary border border-dark text-center text-light">
+            <h2>This site is copyright</h2>
+            <p>@NHMunna 2021</p>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Col, Container, Image, Navbar, Row } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import img from "../../images/backtoschool.jpg";
 
 const Header = () => {
     return (
-        <div>
-            <nav>
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/teacher">Teachers</NavLink>
-                <NavLink to="/courses">Courses</NavLink>
-                <NavLink to="/about">About us</NavLink>
-            </nav>
+        <div >
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <nav className="mx-auto">
+                        <NavLink className="mx-5 text-decoration-none" to="/home">Home</NavLink>
+                        <NavLink className="mx-5 text-decoration-none" to="/teacher">Teachers</NavLink>
+                        <NavLink className="mx-5 text-decoration-none" to="/courses">Courses</NavLink>
+                        <NavLink className="mx-5 text-decoration-none" to="/about">About us</NavLink>
+                    </nav>
+                </Container>
+            </Navbar>
+            <Container>
+                <Image src={img} className="w-100 img-fluid" fluid />
+            </Container>
         </div>
     );
 };
