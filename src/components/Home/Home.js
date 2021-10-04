073f49f218286c5be8cfe5c4b496ef01aa68e7c4
nhmunna <munna1720.nhm@gmail.com>
2021-10-04 18:00:23 +0600

    add few things

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Course from '../Course/Course';
 
 
@@ -22,6 +23,9 @@ const Home = () => {
                         ></Course>)
                     }
                 </Row>
+                <Link to="/courses" className="text-decoration-none text-dark">
+                    <h5 >see more</h5>
+                </Link>
             </Container>
         </div>
     );
