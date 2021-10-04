e2442ad4e804e18288aa5bbf19260a547c1f5c1a
nhmunna <munna1720.nhm@gmail.com>
2021-10-04 18:35:46 +0600

    color correction

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
             <Navbar bg="light" expand="lg">
                 <Container>
                     <nav className="mx-auto">
-                        <NavLink className="mx-5 text-decoration-none" to="/home">Home</NavLink>
-                        <NavLink className="mx-5 text-decoration-none" to="/teacher">Teachers</NavLink>
-                        <NavLink className="mx-5 text-decoration-none" to="/courses">Courses</NavLink>
-                        <NavLink className="mx-5 text-decoration-none" to="/about">About us</NavLink>
+                        <NavLink className="mx-5 text-decoration-none text-dark" to="/home">Home</NavLink>
+                        <NavLink className="mx-5 text-decoration-none text-dark" to="/teacher">Teachers</NavLink>
+                        <NavLink className="mx-5 text-decoration-none text-dark" to="/courses">Courses</NavLink>
+                        <NavLink className="mx-5 text-decoration-none text-dark" to="/about">About us</NavLink>
                     </nav>
                 </Container>
             </Navbar>
